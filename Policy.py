81f954a4aaf5828a4cd6b7b5eb30249a0a848241
krocki <kmrocki@us.ibm.com>
2016-12-28 20:08:33 -0800

    nn

--- Policy.py
+++ Policy.py
@@ -2,9 +2,10 @@
 # @Author: krocki
 # @Date:   2016-12-23 17:56:26
 # @Last Modified by:   krocki
-# @Last Modified time: 2016-12-23 18:18:40
+# @Last Modified time: 2016-12-28 19:14:05
 
 import numpy as np
+from NN import *
 
 class Policy:
 
@@ -14,10 +15,40 @@ class Policy:
 	def forward(self, inputs):
 		raise NotImplementedError()
 
-class ReactiveCartPolicy:
+class NeuralPolicy(Policy):
+
+	nn = []
+	batchsize = 1
 
 	def __init__(self):
-		pass
+		Policy.__init__(self)
+
+		layers = [ 
+			Linear(4, 10, self.batchsize),
+			ReLU(10, 10, self.batchsize), 
+			Linear(10, 2, self.batchsize), 
+			Softmax(2, 1, self.batchsize)
+		]
+
+		self.nn = NN(layers)
+
+	def forward(self, inputs):
+		action = self.nn.forward(inputs)
+		print 'actions \n' + str(action)
+		return sample(action)
+
+class RandomPolicy(Policy):
+
+	def __init__(self):
+		Policy.__init__(self)
+
+	def forward(self, inputs):
+		return np.random.randint(2)
+
+class ReactiveCartPolicy(Policy):
+
+	def __init__(self):
+		Policy.__init__(self)
 
 	def forward(self, inputs):
 		if inputs[3] < 0:
@@ -25,3 +56,18 @@ class ReactiveCartPolicy:
 		if inputs[3] > 0:
 			action = 1
 		return action
+
+def sample(probs):
+
+	print probs
+	cdf = np.cumsum(probs)
+	r = np.random.rand(1)
+
+	idx = 0
+
+	for i in range(probs.shape[0]):
+		if (cdf[i] >= r[0]):
+			idx = i
+			break
+	print idx
+	return idx
--- cartpole.py
+++ cartpole.py
@@ -2,30 +2,29 @@
 # @Author: kmrocki
 # @Date:   2016-12-21 09:39:30
 # @Last Modified by:   krocki
-# @Last Modified time: 2016-12-23 18:14:49
+# @Last Modified time: 2016-12-28 19:12:44
 
 # based on tutorial from https://gym.openai.com/docs
 
 import gym
 from Policy import *
+import numpy as np
 
 env = gym.make('CartPole-v0')
 
-rp = ReactiveCartPolicy()
+policy = NeuralPolicy()
 
 for i_episode in range(20):
 	observation = env.reset()
-	for t in range(1000):
-		env.render()
-		print(observation)
+	for t in range(100):
 
-		action = rp.forward(observation)
-		# plug something in here:
-		# action = env.action_space.sample()
-		#
+		env.render()
+		print 'observations \n' + str(observation)
+
+		action = policy.forward(np.expand_dims(observation, axis=1))
 
 		observation, reward, done, info = env.step(action)
 
-		if done:
-		    print("Episode finished after {} timesteps".format(t+1))
-		    break
+		# if done:
+		#     print("Episode finished after {} timesteps".format(t+1))
+		#     break
