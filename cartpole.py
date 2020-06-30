3f6e518274ce49ba9a278d4c3e2a97230dbe6c0c
Kamil Rocki <kamil.rocki@gmail.com>
2020-06-30 16:17:30 -0700

    py3

--- cartpole.py
+++ cartpole.py
@@ -8,7 +8,7 @@
 # https://gist.github.com/karpathy/a4166c7fe253700972fcbc77e4ea32c5
 
 import numpy as np
-import cPickle as pickle
+import pickle
 import gym
 from gym import wrappers
 
@@ -49,23 +49,23 @@ else:
   model['W1'] = np.random.randn(H,D) / np.sqrt(D) # "Xavier" initialization
   model['W2'] = np.random.randn(A,H) / np.sqrt(H)
   
-grad_buffer = { k : np.zeros_like(v) for k,v in model.iteritems() } # update buffers that add up gradients over a batch
-rmsprop_cache = { k : np.zeros_like(v) for k,v in model.iteritems() } # rmsprop memory
+grad_buffer = { k : np.zeros_like(v) for k,v in model.items() } # update buffers that add up gradients over a batch
+rmsprop_cache = { k : np.zeros_like(v) for k,v in model.items() } # rmsprop memory
 
 def softmax(x):
-	e = np.exp(x);
-	sums = np.sum(e, axis=0);
-	return e / sums;
+  e = np.exp(x);
+  sums = np.sum(e, axis=0);
+  return e / sums;
 
 def discount_rewards(r):
   """ take 1D float array of rewards and compute discounted reward """
   discounted_r = np.zeros_like(r)
   running_add = 0
-  for t in reversed(xrange(0, r.size)):
-	# if r[t] != 0: running_add = 0 # reset the sum, since this was a game boundary (pong specific!)
-	running_add = running_add * gamma + r[t]
-	discounted_r[t] = running_add
-  return discounted_r
+  for t in reversed(range(0, r.size)):
+  # if r[t] != 0: running_add = 0 # reset the sum, since this was a game boundary (pong specific!)
+    running_add = running_add * gamma + r[t]
+    discounted_r[t] = running_add
+    return discounted_r
 
 def policy_forward(x):
   h = np.dot(model['W1'], x)
@@ -85,21 +85,21 @@ def policy_backward(eph, epdlogp):
 
 def sample(probs):
 
-	r = np.random.rand(1)
-	idx = 0
+  r = np.random.rand(1)
+  idx = 0
 
-	if probs.shape[0] == 1:
-		if r < probs[0]: idx = 0
-		else: idx = 1
+  if probs.shape[0] == 1:
+    if r < probs[0]: idx = 0
+    else: idx = 1
 
-	else:
-		cdf = np.cumsum(probs)
-		for i in range(probs.shape[0]):
-			if (cdf[i] >= r[0]):
-				idx = i
-				break
+  else:
+    cdf = np.cumsum(probs)
+    for i in range(probs.shape[0]):
+      if (cdf[i] >= r[0]):
+        idx = i
+        break
 
-	return idx
+  return idx
 
 t = 0
 while episode_number < episodes:
@@ -126,36 +126,36 @@ while episode_number < episodes:
   drs.append(reward) # record reward (has to be done after we call step() to get reward for previous action)
 
   if done: # an episode finished
-	t = 0
-	episode_number += 1
+    t = 0
+    episode_number += 1
 
-	# stack together all inputs, hidden states, action gradients, and rewards for this episode
-	epx = np.vstack(xs)
-	eph = np.vstack(hs)
-	epdlogp = np.vstack(dlogps)
-	epr = np.vstack(drs)
-	xs,hs,dlogps,drs = [],[],[],[] # reset array memory
+    # stack together all inputs, hidden states, action gradients, and rewards for this episode
+    epx = np.vstack(xs)
+    eph = np.vstack(hs)
+    epdlogp = np.vstack(dlogps)
+    epr = np.vstack(drs)
+    xs,hs,dlogps,drs = [],[],[],[] # reset array memory
 
-	# compute the discounted reward backwards through time
-	discounted_epr = discount_rewards(epr)
-	discounted_epr -= np.mean(discounted_epr)
-	discounted_epr /= np.std(discounted_epr)
+    # compute the discounted reward backwards through time
+    discounted_epr = discount_rewards(epr)
+    discounted_epr -= np.mean(discounted_epr)
+    discounted_epr /= np.std(discounted_epr)
 
-	epdlogp *= discounted_epr # modulate the gradient with advantage (PG magic happens right here.)
-	grad = policy_backward(eph, epdlogp)
-	for k in model: 
-		grad_buffer[k] += grad[k] # accumulate grad over batch
+    epdlogp *= discounted_epr # modulate the gradient with advantage (PG magic happens right here.)
+    grad = policy_backward(eph, epdlogp)
+    for k in model: 
+      grad_buffer[k] += grad[k] # accumulate grad over batch
 
-	if episode_number % batch_size == 0:
-	  for k,v in model.iteritems():
-		g = grad_buffer[k] # gradient
-		rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g**2
-		model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
-		grad_buffer[k] = np.zeros_like(v) # reset batch gradient buffer
+    if episode_number % batch_size == 0:
+      for k,v in model.items():
+        g = grad_buffer[k] # gradient
+        rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g**2
+        model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
+        grad_buffer[k] = np.zeros_like(v) # reset batch gradient buffer
 
-	running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
-	print 'resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward)
-	if episode_number % 10 == 0: pickle.dump(model, open('cartpole.p', 'wb'))
-	history.append(running_reward)
-	reward_sum = 0
-	observation = env.reset() # reset env
+    running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
+    print('resetting env. episode reward total was {:}. running mean: {:}'.format(reward_sum, running_reward))
+    if episode_number % 10 == 0: pickle.dump(model, open('cartpole.p', 'wb'))
+    history.append(running_reward)
+    reward_sum = 0
+    observation = env.reset() # reset env
